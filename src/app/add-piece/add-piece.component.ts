ac939194f3a96dcf418264f06bff197fe45c24a7
oussema Mhamed <120645606+oussema445@users.noreply.github.com>
2023-10-26 09:53:46 +0200

    Update add-piece.component.ts

--- src/app/add-piece/add-piece.component.ts
+++ src/app/add-piece/add-piece.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Piece } from '../model/piece.model';
 import { PieceService } from '../services/piece.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-piece',
   templateUrl: './add-piece.component.html',
@@ -10,7 +10,7 @@ import { PieceService } from '../services/piece.service';
 export class AddPieceComponent implements OnInit {
 
   newPiece = new Piece();
-  constructor(private pieceService: PieceService) { }
+  constructor(private pieceService: PieceService,private router :Router) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +18,7 @@ export class AddPieceComponent implements OnInit {
   addPiece() {
     
     this.pieceService.ajouterPiece(this.newPiece);
+    this.router.navigate(['pieces']);
   }
 
 }
